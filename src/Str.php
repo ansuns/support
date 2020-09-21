3c8b25df099095bfd575ce3807797bfc5e93efd2
阿深 <251024883@qq.com>
2020-09-21 08:13:29 +0800

    fix

--- src/Str.php
+++ src/Str.php
@@ -403,7 +403,7 @@ class Str
     /**
      * Convert string's encoding.
      *
-     * @author yansongda <me@yansonga.cn>
+     * @author ansuns
      */
     public static function encoding(string $string, string $to = 'utf-8', string $from = 'gb2312'): string
     {
